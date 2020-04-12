3e278c046bac2d9d02f36a5594c70f03b9160d51
Oliver Barnum <oliverbarnum32@gmail.com>
2020-04-12 14:19:45 -0700

    Add explanation for develop

--- develop.ps1
+++ develop.ps1
@@ -1,13 +1,5 @@
-# develop: 
-# 	cp -a ./mod/. C:/Users/Oliver/Documents/Paradox\ Interactive/Stellaris/mod/star-wars-ethics
-# 	cp ./descriptor.mod C:/Users/Oliver/Documents/Paradox\ Interactive/Stellaris/mod/star-wars-ethics.mod
-
-# undevelop:
-# 	rm -rf C:/Users/Oliver//Documents/Paradox\ Interactive/Stellaris/mod/star-wars-ethics
-# 	rm C:/Users/Oliver/Documents/Paradox\ Interactive/Stellaris/mod/star-wars-ethics.mod
-
-# redevelop: undevelop develop
-
+# To use:
+# . ./develop.ps1
 
 function develop {
     Copy-Item "./descriptor.mod" -Destination "C:\Users\Oliver\Documents\Paradox Interactive\Stellaris\mod\star-wars-ethics.mod"
